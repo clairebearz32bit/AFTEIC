a5f67a8b048d84b70e20d4f1e6a380d375f38230
Claire King <donaldtgina@gmail.com>
2020-05-04 10:52:32 -0600

    Display Features

--- commit.sh
+++ commit.sh
@@ -3,6 +3,4 @@ git add .
 git commit -m "$1"
 git remote add origin https://github.com/drunkpolishbear/AFTEIC.git
 git remote -v
-git fetch origin master
-git merge origin master
-git push origin master
+git push master origin
